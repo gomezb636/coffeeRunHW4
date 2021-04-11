bd03c27688a3784663b51937226f29a1ca774daf
Brenda Gomez <brendagomez@Brendas-MacBook-Pro.local>
2021-04-10 23:11:03 -0700

    Homework 4 due 4/10

---
+++ scripts/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAcnnJfmP4fIYwk9htCse7G8_w0kRqipnY",
+    authDomain: "coffeerun-55712.firebaseapp.com",
+    databaseURL: "https://coffeerun-55712-default-rtdb.firebaseio.com",
+    projectId: "coffeerun-55712",
+    storageBucket: "coffeerun-55712.appspot.com",
+    messagingSenderId: "210511464492",
+    appId: "1:210511464492:web:207ef0c4c0f64d223ae989",
+    measurementId: "G-T79GT7J8YT"
+  };
